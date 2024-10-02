245ca87238ae9937c5d6a315b044b0a8d73d0f16
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-10-03 02:55:53 +0400

    Initial: 03.10.2024

---
+++ HomeTasks/HomeTasks/Program.cs
@@ -0,0 +1,99 @@
+﻿#region EnBoyukEded
+
+//Verilmish 2 mertebeli ededin en boyuk reqemini tapan algorithm misal: input 25, output 5
+
+//Console.WriteLine("Enter your number:");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int newnum1 = number % 10;
+//int newnum2 = number / 10;
+
+//if (number < 10 || number > 99)
+//{
+//    Console.WriteLine("Wrong input, please enter 2 mertebeli number"); //2 mertebeli eng dilinde bilmirem nedi :D
+//}
+//else
+//{
+//    if (newnum1 > newnum2)
+//    {
+//        Console.WriteLine("Biggest number: " + newnum1);
+//    }
+//    else
+//    {
+//        Console.WriteLine("Biggest number: " + newnum2);
+//    }
+//}
+#endregion
+
+#region SadeMurekkeb
+
+//Verilmish ededin sade ve ya murekkeb oldugunu tapan alqoritm
+
+//Console.WriteLine("Enter the number:");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int count = 0;
+
+//if (number == 1)
+//{
+//    Console.WriteLine("1 is neither prime nor composite.");
+//}
+//else if (number <= 0)
+//{
+//    Console.WriteLine("Wrong input, enter number greater than 0");
+//}
+
+//for (int i = 1; i <= number; i++)
+//{
+//    if (number % i == 0)
+//    {
+//        count++;
+//    }
+//}
+
+
+//if (count == 2)
+//{
+//    Console.WriteLine("The number is Prime");
+//}
+//else if (count > 2)
+//{
+//    Console.WriteLine("The number is Composite");
+//}
+
+#endregion
+
+#region 2Quvvet
+
+//verilmish ededin 2 quvveti olub olmadigini tapan alqoritm
+//misal:input 6, output quvveti deyil. input 8 output quvvetidir
+
+//Console.WriteLine("Enter the number: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//if (number == 1)
+//{
+//    Console.WriteLine("The number is not power of 2");
+//}
+//else
+//{
+//    while (number > 1)
+//    {
+//        if (number % 2 != 0)
+//        {
+//            break;
+//        }
+//        number /= 2;
+//    }
+
+//    if (number == 1)
+//    {
+//        Console.WriteLine("The number is power of 2.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("The number is not power of 2.");
+//    }
+//}
+
+#endregion
+
+
